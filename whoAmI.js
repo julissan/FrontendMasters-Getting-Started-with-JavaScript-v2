e44cfcacf05276be3b90f7e95884779d7b90378d
Juan David Lis <julissan@plogran.edu.co>
2022-01-21 15:40:00 -0500

    Who Am I

---
+++ whoAmI.js
@@ -0,0 +1,9 @@
+var student = "Juan David Lis";
+var githubUsername = "julissan";
+var age = 24;
+
+function whoAmI(myName, myUsername, myAge){
+    console.log("Hi, I'm " + myName + ", my username on github is " + myUsername + " and I'm " + myAge + " years old.")
+}
+
+whoAmI(student, githubUsername, age);
